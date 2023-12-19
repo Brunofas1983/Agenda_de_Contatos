55f39c3778558e929a409dc2d25a22d6aefca1cb
Bruno <brunoamarante83@gmail.com>
2023-12-19 17:03:22 -0300

    atualização projeto

--- main.js
+++ main.js
@@ -4,13 +4,28 @@ const atividadenome = [];
 const atividadetel = [];
 let linhas = '';
 
+const handlePhone = (event) => {
+    let input = event.target
+    input.value = phoneMask(input.value)
+}
+
+const phoneMask = (value) => {
+    if (!value) return ""
+    value = value.replace(/\D/g,'')
+    value = value.replace(/(\d{2})(\d)/,"($1) $2")
+    value = value.replace(/(\d)(\d{4})$/,"$1-$2")
+    return value
+}
+
 
 form.addEventListener('submit', function(e) {
     e.preventDefault()
-
-    validacampos();
     
+    validacampos();
+
 })
+
+
 
 function validacampos() {
         const inputnome = document.getElementById('nome');
@@ -39,4 +54,5 @@ function validacampos() {
         inputtel = '';
 
 }
+
 
